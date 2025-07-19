f4ab15762666cb85194ed2764a947d4edff30d14
eriker75 <eribertmarquez75@gmail.com>
2025-07-19 11:21:18 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { MembershipsModule } from './memberships/memberships.module';
+import { TasksModule } from './tasks/tasks.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    ProjectsModule,
+    MembershipsModule,
+    TasksModule,
+    SubscriptionsModule,
+    NotificationsModule,
+    AuthModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
